3236b7fd66a80b6c7757b3b4ef26cb4f2722a91a
highkim0922 <shineest96@gmail.com>
2022-06-22 01:34:31 +0900

    modify : properties 수정

--- src/test/java/com/tube/airbnb/AirbnbApplicationTests.java
+++ src/test/java/com/tube/airbnb/AirbnbApplicationTests.java
@@ -3,7 +3,8 @@ package com.tube.airbnb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" +
+		"classpath:/application-test.yml")
 class AirbnbApplicationTests {
 
 	@Test
