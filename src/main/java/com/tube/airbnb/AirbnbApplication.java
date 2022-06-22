ab8cb6d1cae98c3c49d47206fdbf369624af27a5
highkim0922 <shineest96@gmail.com>
2022-06-23 02:47:06 +0900

    modify : 외부 yml 설정 추가

--- src/main/java/com/tube/airbnb/AirbnbApplication.java
+++ src/main/java/com/tube/airbnb/AirbnbApplication.java
@@ -1,13 +1,17 @@
 package com.tube.airbnb;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class AirbnbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AirbnbApplication.class, args);
+		new SpringApplicationBuilder(AirbnbApplication.class)
+				.properties("spring.config.location=" +
+						"classpath:application.yml," +
+						"optional:/config/application-real-db.yml")
+				.run(args);
 	}
 
 }
